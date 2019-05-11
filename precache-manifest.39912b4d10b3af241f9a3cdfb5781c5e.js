eb75efa6105ce44b40894c803b53b5b566ce526e
Denis Radchenko <denys.radchenko.v@gmail.com>
2019-05-11 19:34:50 +0200

    Updates

---
+++ precache-manifest.39912b4d10b3af241f9a3cdfb5781c5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71ecebf8e0ea045697588e287fd02ed",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "eceb367d700af707a291",
+    "url": "/weather-app-react/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "da8f48808ca9ab22d854",
+    "url": "/weather-app-react/static/css/main.d97fd599.chunk.css"
+  },
+  {
+    "revision": "eceb367d700af707a291",
+    "url": "/weather-app-react/static/js/2.1b0c5b94.chunk.js"
+  },
+  {
+    "revision": "da8f48808ca9ab22d854",
+    "url": "/weather-app-react/static/js/main.0fa122b3.chunk.js"
+  },
+  {
+    "revision": "b246a4fc453f9ee8e4e8",
+    "url": "/weather-app-react/static/js/runtime~main.a31b442a.js"
+  }
+]);
